ad904ea74ce1d438410fbc9ec5667998518edb18
Myridian Star <ameenkhan1208@gmail.com>
2020-05-31 16:03:48 +0800

    ANOTHER ONE

---
+++ WebAPI 2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPI_2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebAPI 2/Models/Product.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product Get(int id);
+        Product Add(Product item);
+        void Remove(int id);
+        bool Update(Product item);
+    }
+
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product> products = new List<Product>();
+        private int _nextId = 1;
+
+        public ProductRepository()
+        {
+            Add(new Product { Name = "Tomato soup", Category = "Groceries", Price = 1.39M });
+            Add(new Product { Name = "Yo-yo", Category = "Toys", Price = 3.75M });
+            Add(new Product { Name = "Hammer", Category = "Hardware", Price = 16.99M });
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return products;
+        }
+
+        public Product Get(int id)
+        {
+            return products.Find(p => p.Id == id);
+        }
+
+        public Product Add(Product item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+
+            item.Id = _nextId++;
+            products.Add(item);
+            return item;
+        }
+
+        public void Remove(int id)
+        {
+            products.RemoveAll(p => p.Id == id);
+        }
+
+        public bool Update(Product item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item");
+            }
+            int index = products.FindIndex(p => p.Id == item.Id);
+            if (index == -1)
+            {
+                return false;
+            }
+            products.RemoveAt(index);
+            products.Add(item);
+            return true;
+        }
+    }
+
+
+}
