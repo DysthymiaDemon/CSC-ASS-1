7094d60df2619c1b30a5f8adf7af7f04090b6eac
Myridian Star <ameenkhan1208@gmail.com>
2020-06-03 17:08:09 +0800

    Done the first one

---
+++ FirstTalentExercise/WebApplication1/script.js
@@ -0,0 +1,38 @@
+$('#search').keyup(function () {
+    //get data from json file
+    var urlForJson = "data.json";
+
+
+    //get data from Restful web Service in development environment
+    //var urlForJson = "http://localhost:9000/api/talents";
+
+    //get data from Restful web Service in production environment
+    //var urlForJson= "http://csc123.azurewebsites.net/api/talents";
+
+    //Url for the Cloud image hosting
+    var urlForCloudImage = "https://res.cloudinary.com/myridianstar/image/upload/v1562897665/samples/cloudinary-group.jpg";
+
+    var searchField = $('#search').val();
+    var myExp = new RegExp(searchField, "i");
+    $.getJSON(urlForJson, function (data) {
+        var output = '<ul class="searchresults">';
+        $.each(data, function (key, val) {
+            //for debug
+            console.log(data);
+            if ((val.Name.search(myExp) != -1) ||
+			(val.Bio.search(myExp) != -1)) {
+                output += '<li>';
+                output += '<h2>' + val.Name + '</h2>';
+                //get the absolute path for local image
+                //output += '<img src="images/'+ val.ShortName +'_tn.jpg" alt="'+ val.Name +'" />';
+
+                //get the image from cloud hosting
+                output += '<img src=' + urlForCloudImage + val.ShortName + "_tn.jpg alt=" + val.Name + '" />';
+                output += '<p>' + val.Bio + '</p>';
+                output += '</li>';
+            }
+        });
+        output += '</ul>';
+        $('#update').html(output);
+    }); //get JSON
+});
