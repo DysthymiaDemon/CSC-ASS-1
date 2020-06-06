6d7e5aef718df0752ae02ce3a8f515c1d7e4375c
Myridian Star <ameenkhan1208@gmail.com>
2020-06-06 15:17:44 +0800

    Can't understand how to complete this task

--- StripeApplication/Controllers/HomeController.cs
+++ StripeApplication/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -26,5 +27,58 @@ namespace StripeApplication.Controllers
 
             return View();
         }
+
+
+        public ActionResult Charge()
+        {
+            ViewBag.Message = "Learn how to process payments with Stripe";
+            return View(new StripeChargeModel());
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Charge(StripeChargeModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+
+
+            var chargeId = await ProcessPayment(model);
+            // You should do something with the chargeId --> Persist it maybe?
+
+            return View("Index");
+        }
+
+
+
+        private static async Task<string> ProcessPayment(StripeChargeModel model)
+        {
+            return await Task.Run(() =>
+            {
+                var myCharge = new StripeChargeCreateOptions
+                {
+                    // convert the amount of £12.50 to pennies i.e. 1250
+                    Amount = (int)(model.Amount * 100),
+                    Currency = "gbp",
+                    Description = "Description for test charge",
+                    Source = new StripeSourceOptions
+                    {
+                        TokenId = model.Token
+                    }
+                };
+
+                var chargeService = new StripeChargeService("your private key here");
+                var stripeCharge = chargeService.Create(myCharge);
+
+                return stripeCharge.Id;
+            });
+        }
+
+
     }
 }
