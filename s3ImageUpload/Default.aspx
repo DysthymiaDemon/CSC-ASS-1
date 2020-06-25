6dd7ad2dca3825f5e1817041ab5d8849e29146a7
Myridian Star <ameenkhan1208@gmail.com>
2020-06-26 02:40:07 +0800

    WOAH! YANKEE WITH NO BRIM!

---
+++ s3ImageUpload/Default.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="s3ImageUpload._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>ASP.NET</h1>
+        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
+        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+    </div>
+
+    <br />
+    <asp:FileUpload ID="FileUpload1" runat="server" accept=".png,.jpg,.jpeg,.gif" />
+    <asp:Button ID="btnUpload" runat="server" Text="Upload Image"
+        OnClick="btnUpload_Click" /><hr />
+    <asp:Image ID="Image1" Visible="false" runat="server" Height="100" Width="100" />
+
+    <asp:TextBox ID="ImageURL" Visible="false" runat="server" Height="25" Width="10000"></asp:TextBox>
+    <asp:TextBox ID="bitlyURL" Visible="false" runat="server" Height="25" Width="1000"></asp:TextBox>
+
+    <div class="row">
+        <div class="col-md-4">
+            <h2>Getting started</h2>
+            <p>
+                ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
+            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
+            </p>
+            <p>
+                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
+            </p>
+        </div>
+        <div class="col-md-4">
+            <h2>Get more libraries</h2>
+            <p>
+                NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.
+            </p>
+            <p>
+                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
+            </p>
+        </div>
+        <div class="col-md-4">
+            <h2>Web Hosting</h2>
+            <p>
+                You can easily find a web hosting company that offers the right mix of features and price for your applications.
+            </p>
+            <p>
+                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
+            </p>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ s3ImageUpload/Default.aspx.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Newtonsoft.Json;
+
+namespace s3ImageUpload
+{
+    
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            // Our Code Will Go Here
+            // Display the Image
+            {
+                System.IO.Stream stream = FileUpload1.PostedFile.InputStream;
+                System.IO.BinaryReader br = new System.IO.BinaryReader(stream);
+                Byte[] bytes = br.ReadBytes((Int32)stream.Length);
+                string base64String = Convert.ToBase64String(bytes, 0, bytes.Length);
+                Image1.ImageUrl = "data:image/jpeg;base64," + base64String;
+                Image1.Visible = true;
+                GetObjectResponse getResponse;
+                var bucketName = "BUCKET_NAME";
+
+                //// Store to S3
+                //{
+                //    // Do not actually store your IAM credentials in code. EC2 Role is best
+                //    var awsKey = "ACCESS_KEY_ID";
+                //    var awsSecretKey = "SECRET_KEY_ID";
+                //    var bucketRegion = Amazon.RegionEndpoint.APSoutheast1;   // Your bucket region
+                //    var s3 = new AmazonS3Client(awsKey, awsSecretKey, bucketRegion);
+                //    var putRequest = new PutObjectRequest();
+                //    putRequest.BucketName = bucketName;        // Your bucket name
+                //    putRequest.ContentType = "image/jpeg";
+                //    putRequest.InputStream = FileUpload1.PostedFile.InputStream;// key will be the name of the image in your bucket
+                //    putRequest.Key = FileUpload1.FileName;
+                //    putRequest.CannedACL = S3CannedACL.PublicRead;
+                //    PutObjectResponse putResponse = s3.PutObject(putRequest);
+
+                //    var pullRequest = new GetObjectRequest();
+                //    pullRequest.BucketName = bucketName;
+                //    pullRequest.Key = FileUpload1.FileName;
+                //    getResponse = s3.GetObject(pullRequest);
+                //}
+
+                //var imageUrl = "https://"+bucketName+".s3-"+Amazon.RegionEndpoint.APSoutheast1.SystemName+".amazonaws.com/"+FileUpload1.FileName;
+                //var imageUrl = "https://kickthebucketdemo.s3-ap-southeast-1.amazonaws.com/Hassum_Harrod_tn.jpg";
+                //ImageURL.Text = imageUrl;
+                //ImageURL.Visible = true;
+
+                //String bitely = ShortenUrl(imageUrl).Result;
+                String bitely = "bit.ly/2Bdommq";
+                String custom = CustomBackHalf(bitely, FileUpload1.FileName).Result;
+                //method to make custom back half
+                //change below
+                bitlyURL.Text = custom;
+                bitlyURL.Visible = true;
+            }
+        }
+
+        public static async Task<string> ShortenUrl(string url)
+        {
+            string _bitlyToken = "560c63bf3029b375974070eea7ebaf4042c8782a";
+            HttpClient client = new HttpClient();
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post,
+                "https://api-ssl.bitly.com/v4/shorten")
+            {
+                Content = new StringContent($"{{\"long_url\":\"{url}\"}}",
+                                                Encoding.UTF8,
+                                                "application/json")
+            };
+
+            try
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _bitlyToken);
+
+                var response = await client.SendAsync(request).ConfigureAwait(false);
+
+                if (!response.IsSuccessStatusCode)
+                    return string.Empty;
+
+                var responsestr = await response.Content.ReadAsStringAsync();
+
+                dynamic jsonResponse = JsonConvert.DeserializeObject<dynamic>(responsestr);
+                return jsonResponse["link"];
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+
+        public static async Task<string> CustomBackHalf(string bitly, string filename)
+        {
+            string _bitlyToken = "560c63bf3029b375974070eea7ebaf4042c8782a";
+            HttpClient client = new HttpClient();
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post,
+                "https://api-ssl.bitly.com/v4/custom_bitlinks")
+            {
+                Content = new StringContent($"{{\"bitlink_id\":\"{bitly}\",\"custom_bitlink\":\"bit.ly/huehuehue\"}}",
+                                                Encoding.UTF8,
+                                                "application/json")
+            };
+
+            try
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _bitlyToken);
+                Debug.WriteLine(request.Content.ReadAsStringAsync().Result);
+                var response = await client.SendAsync(request).ConfigureAwait(false);
+
+                if (!response.IsSuccessStatusCode)
+                    return string.Empty;
+
+                var responsestr = await response.Content.ReadAsStringAsync();
+
+                dynamic jsonResponse = JsonConvert.DeserializeObject<dynamic>(responsestr);
+                return jsonResponse["link"];
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+
+}
