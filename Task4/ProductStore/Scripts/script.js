357e948794ac40795198144fa66d7cd9085e5bbb
Shaggy <2014.philipleongjunhwa@gmail.com>
2020-06-26 00:42:36 +0800

    Remove timer on Load

--- Task4/ProductStore/Scripts/script.js
+++ Task4/ProductStore/Scripts/script.js
@@ -1,11 +1,4 @@
 $('#search').keyup(function () {
-    //get data from Restful web Service in development environment
-    //var urlForJson = "http://localhost:9000/api/talents";
-
-    //get data from Restful web Service in production environment
-    //var urlForJson= "http://csc123.azurewebsites.net/api/talents";
-
-    //Url for the Cloud image hosting
     var urlForCloudImage = "https://res.cloudinary.com/myridianstar/image/upload/v1593078383/foreigntalent/";
 
     var searchField = $('#search').val();
@@ -24,10 +17,6 @@ $('#search').keyup(function () {
                     (val.Bio.search(myExp) != -1)) {
                     output += '<li>';
                     output += '<h2>' + val.Name + '</h2>';
-                    //get the absolute path for local image
-                    //output += '<img src="images/'+ val.ShortName +'_tn.jpg" alt="'+ val.Name +'" />';
-
-                    //get the image from cloud hosting
                     output += '<img src=' + urlForCloudImage + val.ShortName + "_tn.jpg alt=" + val.Name + '" />';
                     output += '<p>' + val.Bio + '</p>';
                     output += '</li>';
@@ -49,39 +38,14 @@ $('#search').keyup(function () {
         $('#update').html(output);
         hideSpinner();
     }
-
-    // $.getJSON(urlForJson, function (data) {
-    //     var output = '<ul class="searchresults">';
-    //     $.each(data, function (key, val) {
-    //         //for debug
-    //         console.log(data);
-    //         if ((val.Name.search(myExp) != -1) ||
-    //(val.Bio.search(myExp) != -1)) {
-    //             output += '<li>';
-    //             output += '<h2>' + val.Name + '</h2>';
-    //             //get the absolute path for local image
-    //             //output += '<img src="images/'+ val.ShortName +'_tn.jpg" alt="'+ val.Name +'" />';
-
-    //             //get the image from cloud hosting
-    //             output += '<img src=' + urlForCloudImage + val.ShortName + "_tn.jpg alt=" + val.Name + '" />';
-    //             output += '<p>' + val.Bio + '</p>';
-    //             output += '</li>';
-    //         }
-    //     });
 });
 
 const spinner = document.getElementById("spinner");
 
 function showSpinner() {
     spinner.className = "show";
-    //setTimeout(() => {
-    //    spinner.className = spinner.className.replace("show", "");
-    //}, 2000);
 }
 
 function hideSpinner() {
-    //spinner.className = "show";
-    setTimeout(() => {
         spinner.className = spinner.className.replace("show", "");
-    }, 2000);
 }
