d043d7c759bd5093dba69ff0441ed7062f3bb9eb
Shaggy <2014.philipleongjunhwa@gmail.com>
2020-06-26 20:12:12 +0800

    Format Directory

--- Task3/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace Task3.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
